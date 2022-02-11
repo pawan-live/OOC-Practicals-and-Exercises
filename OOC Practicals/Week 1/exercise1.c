0ec0d2987278f233ecc28a41f9f0416ea9b3e55a
pawan-live <pawan.senpura@gmail.com>
2022-02-11 23:28:42 +0530

    exercise 1

--- OOC Practicals/Week 1/exercise1.c
+++ OOC Practicals/Week 1/exercise1.c
@@ -2,9 +2,22 @@
 
 int main() {
    int modMarks[3] = {0};
+   float stuAvg[3] = {0};
+   for (int stu = 0; stu < 3; stu++) {
+      int stuTotal = 0;
+      printf("Student %d:\n", stu + 1);
+      
+      for (int i = 0; i < 3; i++) {
+         printf("Enter module %d marks: ", i + 1);
+         scanf("%d", &modMarks[i]);
+         stuTotal += modMarks[i];
+      }
+
+      stuAvg[stu] = stuTotal/3;
+      puts("");
+   }
    for (int i = 0; i < 3; i++) {
-      printf("Enter module %d marks: ", i + 1);
-      scanf("%d", &modMarks[i]);
+      printf("Student %d average is: %0.2f\n", i+1, stuAvg[i]);
    }
    return 0;
 }
