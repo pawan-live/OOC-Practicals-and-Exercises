9bebde46f661730278b98f47bd36c9dce4fe0675
pawan-live <pawan.senpura@gmail.com>
2022-02-11 23:16:19 +0530

    tested ex 1

--- OOC Practicals/Week 1/exercise1.c
+++ OOC Practicals/Week 1/exercise1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   int modMarks[3] = 0;
+   int modMarks[3] = {0};
    for (int i = 0; i < 3; i++) {
       printf("Enter module %d marks: ", i + 1);
       scanf("%d", &modMarks[i]);
